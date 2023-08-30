2ac756364100b814650f0ebc364d4cce652a9cfa
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-08-30 22:56:30 +0200

    Update: Join page updated with Main Layout

--- src/pages/join.tsx
+++ src/pages/join.tsx
@@ -1,10 +1,13 @@
+import MainLayout from '@layouts/MainLayout';
 import { NextPage } from 'next';
 import React from 'react';
 
 const Join: NextPage = () => {
   return (
-    <div>Join</div>
-  )
+    <MainLayout>
+      <h1>Join</h1>
+    </MainLayout>
+  );
 };
 
 export default Join;
